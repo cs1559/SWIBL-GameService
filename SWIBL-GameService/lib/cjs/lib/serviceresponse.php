5ed0363d2f308f2a3062be3f5ae6869266734d0c
Admin <Admin@LAPTOP-5CFK99FO>
2017-10-25 20:02:42 -0500

    initial

---
+++ SWIBL-GameService/lib/cjs/lib/serviceresponse.php
@@ -0,0 +1,28 @@
+<?php
+namespace cjs\lib;
+
+use cjs\lib\Error;
+
+class ServiceResponse {
+    
+    const SUCCESS = true;
+    const FAIL = false;
+
+    var $errors = array();
+    var $data = null;
+
+    public function setData($content) {
+        $this->data = $content;
+    }
+    public function getData() {
+        return $this->data;
+    }
+    
+    public function addError(Error $error) {
+        $this->errors[] = $error;
+    }
+    
+    public function getErrors() {
+        return $this->errors;
+    }
+}
