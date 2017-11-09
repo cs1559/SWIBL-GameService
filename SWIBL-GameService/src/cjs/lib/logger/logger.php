6bb1a5c87add2ee44bd8aabfa29d71f25fdc34b9
Admin <Admin@LAPTOP-5CFK99FO>
2017-11-08 19:43:49 -0600

    add Log Level attribute

--- SWIBL-GameService/src/cjs/lib/logger/logger.php
+++ SWIBL-GameService/src/cjs/lib/logger/logger.php
@@ -3,6 +3,24 @@ namespace cjs\lib\logger;
 
 abstract class Logger {
     
+    var $level = 1;
+    
+    /**
+     * @return the $level
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * @param number $level
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+    }
+
     abstract function info($msg);
     abstract function warning($msg);
     abstract function error($msg);
