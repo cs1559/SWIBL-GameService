8fd403878fd88e1172d09361adcd50ae5d9b3eab
Admin <Admin@LAPTOP-5CFK99FO>
2017-11-11 16:30:50 -0600

    added logging and correct response messages.

--- SWIBL-GameService/src/swibl/actions/getteamscheduleaction.php
+++ SWIBL-GameService/src/swibl/actions/getteamscheduleaction.php
@@ -15,37 +15,44 @@ class GetTeamScheduleAction
        $this->container = $container;
    }
    
-   public function __invoke($request, $response, $args) {
+   public function __invoke($request,  $response, $args) {
   
+       $service = GameService::getInstance();
+       $body = $request->getBody();
+       $content = $body->getContents();
+       
+       $logger = $service->getLogger();
+       $logger->info("POST /schedule " . $request->getUri() );
        
        $uri = $request->getUri();
        
        $params = $request->getQueryParams();
        
-       if (isset($params["season"])){
-           $season = $params["season"];
-       } else {
-           $svcresponse = GameServiceResponse::getInstance(400, "Season ID is missing from the request - " . "GET /schedule/{teamid}");
-           $response->write(json_encode($svcresponse));
-           return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
-       }
+//        if (isset($params["season"])){
+//            $season = $params["season"];
+//        } else {
+//            $svcresponse = GameServiceResponse::getInstance(400, "Season ID is missing from the request - " . "GET /schedule/{teamid}");
+//            $response->write(json_encode($svcresponse));
+//            return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+//        }
        
        $teamid = $request->getAttribute("teamid");
-       $dao = new \swibl\GamesDAO();
+       $season = $request->getAttribute("seasonid");
+       
+       $dao = \swibl\GamesDAO::getInstance($service->getDatabase());
        try {
            $results = $dao->getGameSchedule($teamid, $season);
+           
            $games = \swibl\GameHelper::bindArray($results);
-           $svcresponse = GameServiceResponse::getInstance(200, "Schedule Retrieved");
+                     
+           $svcresponse = GameServiceResponse::getInstance(200, "Schedule Retrieved [" . sizeof($games) . "]");
            $svcresponse->setData($games);
-           //             $svcresponse->setCode(200);
-           //             $svcresponse->setData($games);
            $response->withHeader('Content-Type', 'application/json');
            $response->write(json_encode($svcresponse));
        }
        catch (\cjs\lib\exception\RecordNotFoundException $e) {
-           $svcresponse = new GameServiceResponse();
-           $svcresponse->setCode(400);
-           $svcresponse->setMessage($e->getMessage());
+           $logger->info("TEAM SCHEDULE NOT FOUND - " . $request->getUri());
+           $svcresponse = new GameServiceResponse(400, $e->getMessage());
            $response->write(json_encode($svcresponse));
            return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
        }
@@ -54,9 +61,7 @@ class GetTeamScheduleAction
            $error->setSourcefile("file: " . $e->getFile() . " Line#: " . $e->getLine());
            $error->setMethod("GET /{id}");
            $error->setInternalMessage($e->getMessage());
-           $svcresponse = new GameServiceResponse();
-           $svcresponse->setCode(400);
-           $svcresponse->setMessage($e->getMessage());
+           $svcresponse = new GameServiceResponse(400, $e->getMessage());
            $svcresponse->addError($error);
            $response->write(json_encode($svcresponse));
            return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
