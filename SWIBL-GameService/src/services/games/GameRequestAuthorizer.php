b3943e778e9e6a1c85122da29a975f05047a9e22
Admin <Admin@LAPTOP-5CFK99FO>
2017-11-12 20:32:50 -0600

    implemented checkServiceAuthorization method

--- SWIBL-GameService/src/services/games/GameRequestAuthorizer.php
+++ SWIBL-GameService/src/services/games/GameRequestAuthorizer.php
@@ -1,6 +1,7 @@
 <?php
 namespace swibl\services\games;
 
+use Psr\Http\Message\ServerRequestInterface;
 use swibl\services\games\GameService;
 use swibl\core\RequestAuthorizer;
 use swibl\core\authentication\AuthDAO;
@@ -11,5 +12,7 @@ class GameRequestAuthorizer extends RequestAuthorizer {
         self::setService(GameService::getInstance());
     }
  
-    
+    function checkServiceAuthorizations(ServerRequestInterface $request) {
+        return true;
+    }
 }
